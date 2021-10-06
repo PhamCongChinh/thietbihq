05178474e12892f721f459bf96074d6061adca61
PhamCongChinh <chinhpham1609@gmail.com>
2021-10-06 18:21:15 +0700

    Google tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,13 @@ import 'tailwindcss/tailwind.css'
 import '../public/styles.css'
 import 'react-toastify/dist/ReactToastify.css'
 
+import { useEffect } from 'react'
+import TagManager from 'react-gtm-module'
+
 const MyApp = ({ Component, pageProps }) => {
+    useEffect(() => {
+        TagManager.initialize({ gtmId: 'GTM-K5GSNQW' });
+    }, [])
     const getLayout = Component.getLayout || ((page) => page)
     return getLayout (
         <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
-// eslint-disable-next-line @next/next/no-document-import-in-page
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { GTM_ID } from '../lib/gtm'
+
 class MyDocument extends Document {
     render() {
         return (
@@ -9,14 +8,6 @@ class MyDocument extends Document {
                     <link rel="icon" href="/favicon.ico" />
                 </Head>
                 <body>
-                    <noscript>
-                        <iframe
-                        src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-                        height="0"
-                        width="0"
-                        style={{ display: 'none', visibility: 'hidden' }}
-                        />
-                    </noscript>
                     <Main />
                     <NextScript />
                 </body>
