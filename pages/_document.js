277cf6c52d5baed5e65352d5808af6ba47237eb7
PhamCongChinh <chinhpham1609@gmail.com>
2021-10-12 06:23:00 +0700

    update next 11.1.2

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,11 @@
-/* eslint-disable @next/next/no-document-import-in-page */
+// eslint-disable-next-line @next/next/no-document-import-in-page
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
     render() {
         return (
             <Html lang="vi">
